940f6aefa33b25070f763369eda9d0f971ccc744
emilianogarcia05 <emilianogarcia05@gmail.com>
2020-10-13 20:15:28 -0300

    Push Inicial

---
+++ src/main/java/com/faltauno/ProyectoFinalEggApplication.java
@@ -0,0 +1,13 @@
+package com.faltauno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalEggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalEggApplication.class, args);
+	}
+
+}
