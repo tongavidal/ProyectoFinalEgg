c87141efd6e4e973224d8a9ab2d263d34580c9f8
Raisa Orellana <raisa.orellana.rios@gmail.com>
2020-10-21 20:43:52 -0500

    Controlador Partido

--- src/main/java/com/faltauno/controladores/PartidoControlador.java
+++ src/main/java/com/faltauno/controladores/PartidoControlador.java
@@ -1,12 +1,18 @@
 package com.faltauno.controladores;
 
+import com.faltauno.entidades.Establecimiento;
 import com.faltauno.entidades.Partido;
 import com.faltauno.entidades.Usuario;
+import com.faltauno.enumeraciones.Sexo;
 import com.faltauno.errores.ErrorServicio;
 import com.faltauno.repositorios.PartidoRepositorio;
+import com.faltauno.servicios.EstablecimientoServicio;
 import com.faltauno.servicios.PartidoServicio;
+import java.util.Date;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +30,9 @@ public class PartidoControlador {
 
     @Autowired
     private PartidoServicio partidoServicio;
+    
+    @Autowired
+    private EstablecimientoServicio establecimientoServicio;
 
     @PostMapping("/listar-postulados")
     public String listarpostulados(ModelMap modelo, @RequestParam String idpartido) throws ErrorServicio {
@@ -105,6 +114,28 @@ public class PartidoControlador {
         }
 
         return "listado-postulados";
+    }
+    
+    @GetMapping("/alta-partido")
+    public String registrarPartido(HttpSession session,ModelMap modelo){
+        List<Establecimiento> establecimientos=establecimientoServicio.listaEstablecimientos();
+        modelo.put("establecimientos", establecimientos);
+//        List<Localidad> localidades=localidadServicio.listaLocalidades();
+//        modelo.put("localidades", establecimientos);
+        modelo.put("sexo", Sexo.values());
+        return "HTML";
+    }
+    
+    @PostMapping("/alta-partido-ok")
+    public String registroPartido(HttpSession session,ModelMap modelo,@RequestParam String idLocalidad,@RequestParam Integer cantJugador,@RequestParam String horario,@RequestParam Integer cantVacantes, @RequestParam Double precio, @RequestParam String obsVacante,@RequestParam String obsEstablecimiento,@RequestParam Sexo sex,@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")Date fecha){
+        try{
+            Usuario creador=(Usuario)session.getAttribute("usuariosession");
+            partidoServicio.crearPartido(idLocalidad, cantJugador, Integer.parseInt(horario), cantVacantes, precio,creador, obsVacante, obsEstablecimiento, sex, fecha);
+        }catch(ErrorServicio ex){
+            
+        }
+        
+        return"html";
     }
 
 }
