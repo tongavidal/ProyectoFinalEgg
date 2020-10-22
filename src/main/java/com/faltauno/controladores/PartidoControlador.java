3242db01d18b7dbdc6cb81aae13e2aa87efd217c
vidalgaston <vidalgaston92@gmail.com>
2020-10-22 22:16:37 +0200

    Borre import getmapping

--- src/main/java/com/faltauno/controladores/PartidoControlador.java
+++ src/main/java/com/faltauno/controladores/PartidoControlador.java
@@ -2,12 +2,11 @@
 package com.faltauno.controladores;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/partido")
 public class PartidoControlador {
-    
+ 
     
 }
