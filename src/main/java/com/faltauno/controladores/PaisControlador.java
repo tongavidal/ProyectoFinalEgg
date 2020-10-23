ede96b4e34a370813e145c7fdc5354ea27d37a81
Enrique Ben <enriquebenzano@gmail.com>
2020-10-23 17:32:25 -0300

    loguin, seguridad, accesos a controlador partido

--- src/main/java/com/faltauno/controladores/PaisControlador.java
+++ src/main/java/com/faltauno/controladores/PaisControlador.java
@@ -4,6 +4,7 @@ package com.faltauno.controladores;
 import com.faltauno.errores.ErrorServicio;
 import com.faltauno.servicios.PaisServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ public class PaisControlador {
     @Autowired
     private PaisServicio paisServicio;
     
+    @PreAuthorize("hasAnyRole('ROLE_USUARIO_ADMIN')")
     @PostMapping("/alta-pais")
     public String altapais(ModelMap modelo, @RequestParam(required = false) String nombrepais) throws ErrorServicio {
         try {
---
+++ src/main/java/com/faltauno/seguridad/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.faltauno.seguridad;
+
+import com.faltauno.servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ *
+ * @author CARMEN
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    
+    @Autowired
+    public UsuarioServicio usuarioService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usuarioService).
+                passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/*", "/js/*", "/img/*", "/**").permitAll()
+                .and().formLogin()
+                .loginPage("/usuario/registrarse")
+                .loginProcessingUrl("/usuario/loguinchek")
+                .usernameParameter("email")
+                .passwordParameter("clave")
+                .defaultSuccessUrl("/")
+                .failureUrl("/usuario/usuario?error=error")
+                .permitAll()
+                .and().logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll()
+                .and().csrf()
+                .disable();
+    }
+
+}
